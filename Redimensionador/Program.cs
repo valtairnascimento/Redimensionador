e1a750813630468c71db5504cc2afe05906d51cd
valtair <valtair@VALTAIR>
2024-08-27 10:42:19 -0300

    Adicionar arquivos de projeto.

---
+++ Redimensionador/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Redimensionador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando redimensionador");
+
+
+        }
+    }
+}
